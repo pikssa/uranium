f036ad33be01da3fa4f6e10cf4dd26299f0f3f99
pikssa <puru.14.kaushik@gmail.com>
2022-04-07 12:37:01 +0530

    hi

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,22 +1,22 @@
 const express = require('express');
-const logger=require('../logger/logger')
-const util= require('../util/helper')
-const validater =require('../validater/formatter')
-const validaterr =require('../terminater/arr')
+const logger = require('../logger/logger')
+const util = require('../util/helper')
+const validater = require('../validater/formatter')
+const validaterr = require('../terminater/arr')
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
-logger.log
-  
-   util.printDate()
-    util.printMonth()
-    util.a()
- validater.trim
- validater.lowercase
- validater.uppercase
+   logger.log
 
-validaterr.result
-validaterr.result1
+   util.printDate()
+   util.printMonth()
+   util.a()
+   validater.trim
+   validater.lowercase
+   validater.uppercase
+
+   validaterr.result
+   validaterr.result1
    res.send('My first ever api!')
 });
 
