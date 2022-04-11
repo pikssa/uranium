234eab3bb5c1ebfd12979757901d140cc659da65
pikssa <puru.14.kaushik@gmail.com>
2022-04-11 22:31:03 +0530

    hii

--- src/index.js
+++ src/index.js
@@ -2,14 +2,31 @@ const express = require('express');
 var bodyParser = require('body-parser');
 
 const route = require('./routes/route.js');
+const { default: mongoose } = require('mongoose');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+mongoose.connect("mongodb+srv://lddu818:27o3D6VwW2z1zHMj@cluster0.6gomf.mongodb.net/puru?retryWrites=true&w=majority",{
+    useNewUrlParser: true
+
+})
+.then ( ( ) => console.log("MongoDb is connected"))
+.catch( err => console.log(err) )
+
+
 
 app.use('/', route);
 
 app.listen(process.env.PORT || 3000, function() {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
+
+
+
+
+
+
+
+
